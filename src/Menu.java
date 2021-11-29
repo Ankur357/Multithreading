784b63c23c75cd9b43f92d5cce94f72b5af75515
Ankur357 <ankur.gautam_cs20@gla.ac.in>
2021-11-29 11:27:44 +0530

    Code

---
+++ src/Menu.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Menu {
+    public static void main(String[] args) {
+        System.out.println("Enter \n 1 Checked Exception \n 2 Unchecked Exception");
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        if(a==1){
+            System.out.println("Enter \n 1 SQLException \n 2 IOException \n 3 ClassNotFoundException \n 4 InvocationTargetException \n 5 FileNotfound Exception");
+            int b = sc.nextInt();
+            if(b==1){
+
+            }
+        }
+
+    }
+}
